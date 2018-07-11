8f3dd8b394a2e3c1d64bacdfe68de3e9edfa2441
PolGuixe <pol.guixe@gmail.com>
2018-07-11 16:11:41 +0200

    fixed package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "origen:vulcan-hotjar",
+  name: "origenstudio:vulcan-hotjar",
   version: "0.0.1",
   // Brief, one-line summary of the package.
   summary: "Hotjar integration for VulcanJS",
